221c5b6ae16e342880348e0536d829d9766a2094
Martin/Geno <geno+dev@fireorbit.de>
2019-05-17 20:41:45 +0200

    add json format validation

--- blacklist.sh
+++ blacklist.sh
@@ -1,4 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
 
-exec jq -e ".peers | map(select(.key == \"$PEER_KEY\")) | length == 0" \
+jq -e ".peers | map(select(.key == \"$PEER_KEY\")) | length == 0" \
     "${1:-blacklist.json}" > /dev/null
+
+RET=$?
+
+# exit 0 for invalid files -> allow every fastd peer_key
+if [ $RET -eq 4 ]; then
+	exit 0;
+fi
+exit $RET
